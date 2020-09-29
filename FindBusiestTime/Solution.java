454a96bc0de624b197e28633e5b33bad09f999a3
Akshay Adagale <39808339+AkshayAdagaleGitHub@users.noreply.github.com>
2020-09-29 13:13:22 +0530

    Update Solution.java

--- FindBusiestTime/Solution.java
+++ FindBusiestTime/Solution.java
@@ -53,3 +53,65 @@ class Solution {
   }
 
 }
+
+/*
+Test Case #1
+Input:
+[], 12
+Expected:
+[]
+Actual:
+[]
+
+Test Case #2
+Input:
+[4,4,4], 12
+Expected:
+[]
+Actual:
+[]
+
+Test Case #3
+Input:
+[4,4,4,2], 16
+Expected:
+[]
+Actual:
+[]
+
+Test Case #4
+Input:
+[4,4,4,4], 16
+Expected:
+[4,4,4,4]
+Actual:
+[4, 4, 4, 4]
+
+
+Test Case #5
+Input:
+[2,7,4,0,9,5,1,3], 20
+Expected:
+[0,4,7,9]
+Actual:
+[0, 4, 7, 9]
+
+Test Case #6
+Input:
+[2,7,4,0,9,5,1,3], 120
+Expected:
+[]
+Actual:
+[]
+
+
+Test Case #7
+Input:
+[1,2,3,4,5,9,19,12,12,19], 40
+Expected:
+[4,5,12,19]
+Actual:
+[4, 5, 12, 19]
+
+
+*/
